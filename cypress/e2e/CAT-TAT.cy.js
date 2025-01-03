6616beffeba07b985324dd3fcb886f3f6fcdba2b
brendojn <brendojnio@gmail.com>
2025-01-03 06:50:27 -0300

    Break the app on purpose

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -171,7 +171,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible');
   })
 
-  it.only('testa a página da política de privacidade de forma independente', () => {
+  it('testa a página da política de privacidade de forma independente', () => {
     cy.visit('./src/privacy.html');
 
     cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible');
